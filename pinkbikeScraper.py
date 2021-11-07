992030f4c9b5461902995d4a4adcacdc9c5f28d2
seangaff <gaffnese@gmail.com>
2021-11-07 21:34:48 +0000

    All data points test

--- pinkbikeScraper.py
+++ pinkbikeScraper.py
@@ -4,41 +4,56 @@ import re
 #import pymongo
 from bs4 import BeautifulSoup
 
-#client = pymongo.MongoClient("mongodb+srv://dbTest:<password>@cluster0.fsdva.mongodb.net/personal-site?retryWrites=true&w=majority")
-#db = client.test
+#URL = 'https://www.pinkbike.com/buysell/list/?lat=37.6806&lng=-122.4073&distance=101&category=2'    #local Trail
+URL = 'https://www.pinkbike.com/buysell/list/?region=3&category=2'                                 #All Trail
 
-URL = 'https://www.pinkbike.com/buysell/list/?lat=37.6806&lng=-122.4073&distance=101&category=2'    #local Trail
-#URL = 'https://www.pinkbike.com/buysell/list/?lat=37.6806&lng=-122.4073&distance=101&category=1'   #local DH
-#URL = 'https://www.pinkbike.com/buysell/list/?category=2'                                          #All Trail
-#URL = 'https://www.pinkbike.com/buysell/list/?category=1'                                          #All DH
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
-
-#result = soup.find(id='csid2882151')
-#print (result.text.strip())
-#print(result.prettify())
 bikeElements = soup.find_all('div', class_="bsitem")
+
 for bikeElement in bikeElements:
-    
-    pbId = bikeElement.get('id')
+    pbId = bikeElement.get('id').replace("csid","")
+    print(pbId)
     bikeURL = "https://www.pinkbike.com/buysell/" + pbId
-    #print(bikeElement.text.strip())
     bikeData = bikeElement.find_all('td')
-    title = bikeData[1].a.string
     bikeSpecs = bikeData[1].find_all("div")
-    
-    condition = bikeSpecs[1].text.split(":",1)[1]
-    frameSize = bikeSpecs[2].text.split(":",1)[1]
-    wheelSize = bikeSpecs[3].text.split(":",1)[1]
-    Material = bikeSpecs[4].text.split(":",1)[1]
-    frontTravel = bikeSpecs[5].text.split(":",1)[1]
-    rearTravel = bikeSpecs[6].text.split(":",1)[1]
-    print()
-    '''
-    for spec in bikeSpecs:
-       print(spec.text.strip())
-   '''
-   #test
-    break
-   
+
+    title = bikeData[1].a.string
+    condition = bikeSpecs[1].text.split(":",1)[1].strip()
+    frameSize = bikeSpecs[2].text.split(":",1)[1].strip()
+    wheelSize = bikeSpecs[3].text.split(":",1)[1].strip()
+    Material = bikeSpecs[4].text.split(":",1)[1].strip()
+    frontTravel = bikeSpecs[5].text.split(":",1)[1].strip()
+    rearTravel = bikeSpecs[6].text.split(":",1)[1].strip()
+
+    bikeInfo = bikeData[1].find_all("tr")
+    location = bikeInfo[0].text.strip()
+    seller = bikeInfo[1].text.split(":")[1].split("|")[0].strip()
+    price = int(bikeInfo[2].text.replace("$","").split(" ")[0].strip())
+
+    bikePage = requests.get(bikeURL)
+    bikeSoup = BeautifulSoup(bikePage.content, 'html.parser')   
+    bikeDetails = bikeSoup.find_all("div",class_="buysell-details-column")[1].get_text(strip=True, separator='\n').splitlines()
+
+    postDate = bikeDetails[1]
+    repostDate = bikeDetails[3]
+    forSale = bikeDetails[6]
+    viewCount = int(bikeDetails[8])
+    watchCount = int(bikeDetails[10])
+
+    #break
+
+'''
+class bikeScraper():
+    def iterateSeachPage():
+
+    def scrapeSearchPage(URL):
+
+    def scrapeBikeElement():
+
+    def scrapeListing():
+
+
+if __name__ == '__main__':
+
+'''   
