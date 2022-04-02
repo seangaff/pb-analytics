191e480020869b54f8dbe5ccfb6e1d8f92ebb846
seangaff <gaffnese@gmail.com>
2022-04-03 00:12:40 +0100

    Function templates and scaping edge cases

--- Scraper/pinkbikeScraper.py
+++ Scraper/pinkbikeScraper.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import re
 import datetime
+from datetime import datetime
 from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials
@@ -13,8 +14,8 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-#URL = 'https://www.pinkbike.com/buysell/list/?lat=37.6806&lng=-122.4073&distance=101&category=2'    #local Trail
-URL = 'https://www.pinkbike.com/buysell/list/?region=3&category=2'                                 #All Trail
+#URL = 'https://www.pinkbike.com/buysell/list/?lat=37.6806&lng=-122.4073&distance=101&category=2'       #local Trail
+URL = 'https://www.pinkbike.com/buysell/list/?region=3&category=2'                                      #All Trail
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -37,25 +38,28 @@ for bikeElement in bikeElements:
     title = bikeData[1].a.string
     condition = bikeSpecs[1].text.split(":",1)[1].strip()
     frameSize = bikeSpecs[2].text.split(":",1)[1].strip()
-    wheelSize = bikeSpecs[3].text.split(":",1)[1].strip()
-    Material = bikeSpecs[4].text.split(":",1)[1].strip()
+    wheelSize = bikeSpecs[3].text.split(":",1)[1].replace('"',"").strip()
+    material = bikeSpecs[4].text.split(":",1)[1].strip()
     frontTravel = bikeSpecs[5].text.split(":",1)[1].strip()
     rearTravel = bikeSpecs[6].text.split(":",1)[1].strip()
 
     bikeInfo = bikeData[1].find_all("tr")
     location = bikeInfo[0].text.strip()
-    seller = bikeInfo[1].text.split(":")[1].split("|")[0].replace("Outside+", "").strip()
+    # seller = bikeInfo[1].text.split(":")[1].split("|")[0].replace("Outside+", "").strip()
+    seller = bikeInfo[1].a.text.replace("Outside+", "").strip()
+    print(seller)
     price = int(bikeInfo[2].text.replace("$","").split(" ")[0].strip())
     #if price is in CAD, rough conversion to USD
     if((bikeInfo[2].text.replace("$","").split(" ")[1].strip()) == "CAD"):
         price = price*0.8
+    price = int(price)
 
     #get listing data from Bike's page by pbID
     bikePage = requests.get(bikeURL)
     bikeSoup = BeautifulSoup(bikePage.content, 'html.parser')   
     bikeDetails = bikeSoup.find_all("div",class_="buysell-details-column")[1].get_text(strip=True, separator='\n').splitlines()
-    # postDate = datetime.strptime((bikeDetails[1].split(" ")[0].replace("-"," ")), '%b %d %Y')
-    # repostDate = datetime.strptime((bikeDetails[3].split(" ")[0].replace("-"," ")), '%b %d %Y')
+    postDate = datetime.strptime((bikeDetails[1].split(" ")[0].replace("-"," ")), '%b %d %Y')
+    repostDate = datetime.strptime((bikeDetails[3].split(" ")[0].replace("-"," ")), '%b %d %Y')
     if(bikeDetails[5] == "since"):
         forSale = bikeDetails[6]
         viewCount = int(bikeDetails[8].replace(",", ""))
@@ -70,26 +74,41 @@ for bikeElement in bikeElements:
     doc_ref = db.collection(u'trailEnduro').document(pbID)
     doc_ref.set({
         u'title': title,
-        u'condition': condition,
+        u'location': location,
         u'seller': seller,
         u'price_usd': price,
-        u'location': location,
+        u'spec': {
+            u'condition': condition,
+            u'frame_size': frameSize,
+            u'wheel_size': wheelSize,
+            u'material': material,
+            u'front_travel': frontTravel,
+            u'rear_travel': rearTravel
+        },
+        u'listing': {
+            u'post_date': postDate,
+            u'repost_date': repostDate,
+            u'for_sale': forSale,
+            u'view_count': viewCount,
+            u'watch_count': watchCount,
+        },
     })
-    
-
     break
 
-'''
-class bikeScraper():
-    def iterSeachPage():
+def iterateSeach():
+    print("Iterating through search pages")
 
-    def scrapeSearchPage(URL):
+def scrapeSearchPage(URL):
+    print("Scraping Search Page")
 
-    def scrapeElement():
+def scrapeElement():
+    print("Scraping Element")
 
-    def scrapeListing():
+def scrapeListing():
+    print("Scraping Listing")
 
+def publishData():
+    print("Publishing Data")
 
 if __name__ == '__main__':
-    iterSearchPage(URL)
-'''   
+    iterateSeach()
